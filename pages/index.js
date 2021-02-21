eae700565e435351b9f62c0754f27ab5e6eb18d8
Yoshimasa Suzuki <cognac0004@gmail.com>
2021-02-21 13:39:17 +0900

    added style to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
-export default () => (
+const h1 = {
+  fontSize: '72px',
+  fontWeight: 'bold',
+  textAlign: 'right',
+  letterSpacing: '-8px',
+  color: '#f0f0f0',
+  margin: '-40px 0px'
+}
+const p = {
+  margin: '0px',
+  color: '#666',
+  fontSize: '16px'
+}
+
+const HTML = () => (
   <div>
-    <h1>Hello Next.js</h1>
-    <p>this page is next.js</p>
+    <h1 style={h1}>Next.js</h1>
+    <p style={p}>welcome to next.js!</p>
   </div>
 )
+export default HTML
 
